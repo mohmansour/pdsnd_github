222956c88a7b36ba481e3abd0d99bc2a7636f3ea
bondoq <mohamedmansour.mm317@gmail.com>
2020-01-05 00:13:15 +0200

    refactoring script

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -10,9 +10,9 @@ def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
     Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        city - name of the city to analyze
+        month - name of the month to filter by, or "all"
+        day - name of the day of week to filter by, or "all"
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
@@ -45,9 +45,9 @@ def load_data(city, month, day):
     Loads data for the specified city and filters by month and day if applicable.
 
     Args:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        city - name of the city to analyze
+        month - name of the month to filter by, or "all"
+        day - name of the day of week to filter by, or "all"
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
